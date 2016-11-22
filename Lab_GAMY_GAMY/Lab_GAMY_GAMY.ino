1013f111b0c2c4dcc96c610fb47a64ad865efee9
Kelsey Maes <kelsey@festfeed.com>
2016-11-22 15:52:26 -0800

    Implemented game data send function, started on receive function.

--- Lab_GAMY_GAMY/Lab_GAMY_GAMY.ino
+++ Lab_GAMY_GAMY/Lab_GAMY_GAMY.ino
@@ -51,10 +51,19 @@ namespace Network {
   }
 
   // For internal use
-  void sendData(String data) {
+  void sendData(char* data) {
     client.beginPacket(hostIP,port);
     client.print("{\"game\":\"SimonSays\",\"payload\":\"");
-    client.print(data.c_str());
+    client.print(data);
+    client.print("\"}");
+    client.endPacket();
+  }
+
+  // For internal use
+  void sendData(String* data) {
+    client.beginPacket(hostIP,port);
+    client.print("{\"game\":\"SimonSays\",\"payload\":\"");
+    client.print(data->c_str());
     client.print("\"}");
     client.endPacket();
   }
@@ -96,6 +105,53 @@ namespace Network {
   void sendHandshakeResponse() {
     sendData("HI");
   }
+
+  // Use this to actually send the game data
+  void sendGameData(byte* selections,byte* durations,int length) {
+    String* data = new String("?");
+    
+    *data += "selections=[";
+    for(int i = 0; i < length; ++i) {
+      if(selections[i] == NULL)
+        break;
+      if(i != 0) *data += ',';
+      *data += selections[i];
+    }
+
+    *data += "]&durations=[";
+    for(int i = 0; i < length; ++i) {
+      if(durations[i] == NULL)
+        break;
+      if(i != 0) *data += ',';
+      *data += durations[i];
+    }
+    *data += "]";
+
+    sendData(data);
+    delete data;
+  }
+  
+  // Use this to receive the game data
+  boolean receiveGameData() {
+    String* data = readData();
+    boolean response = false;
+    if(data != NULL && data->startsWith("?")) {
+      data->remove(0,1);
+      String* temp = new String("");
+      for(int i = 0; data->charAt(i) != '='; ++i)
+        *temp += data->charAt(i);
+
+      if(temp->compareTo("selections") == 0) {
+      }
+
+      if(temp->compareTo("durations") == 0) {
+      }
+      
+      delete temp;
+    }
+
+    delete data;
+  }
 }
 
 // declaration section
@@ -108,7 +164,7 @@ int ledPin_2 = 13;
 int ledPin_3 = 12;
 int ledPin_4 = 16;
 int leds[] = {ledPin_1, ledPin_2, ledPin_3, ledPin_4};
-byte seletLed[500];
+byte selectLed[500];
 byte duration[500];
 volatile long lastEncoded = 0;
 volatile long encoderValue = 0;
